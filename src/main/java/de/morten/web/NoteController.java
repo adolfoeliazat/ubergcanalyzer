36e5ed15ec6c30f41e48ec5159479ff609a613f3
Christian Bannes <cbannes@gmail.com>
2013-04-18 23:13:04 +0200

    implementation Note component. Working now

--- src/main/java/de/morten/web/NoteController.java
+++ src/main/java/de/morten/web/NoteController.java
@@ -1,8 +1,12 @@
 package de.morten.web;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 
@@ -13,7 +17,9 @@ import javax.inject.Named;
 @SessionScoped
 public class NoteController implements Serializable {
 	private static final long serialVersionUID = 7199060576374141490L;
-	String text = "";
+	String text = "Enter your notes...";
+	private String fontFamily = "Satisfy";
+	private int fontSize = 16;
 
 	public String getText() {
 		System.out.println("get text");
@@ -25,6 +31,30 @@ public class NoteController implements Serializable {
 		this.text = text;
 	}
 	
+	
+	public String getFontFamily()
+	{
+		return this.fontFamily;
+	}
 
+	public void setFontFamily(final String fontFamily)
+	{
+		System.out.println("setting fontfamily:" + fontFamily);
+		this.fontFamily = fontFamily;
+	}
+	
+	public int getFontSize()
+	{
+		System.out.println("get fontsize");
+		return this.fontSize;
+	}
 
+	public void setFontSize(final int fontSize)
+	{
+	//	UIComponent component = UIComponent.getCurrentComponent(FacesContext.getCurrentInstance());
+	//	org.primefaces.component.selectonelistbox.SelectOneListbox box = (org.primefaces.component.selectonelistbox.SelectOneListbox) component;
+	//	box.getSubmittedValue();
+		System.out.println("setting fontSize:" + fontSize);
+		this.fontSize = fontSize;
+	}
 }
