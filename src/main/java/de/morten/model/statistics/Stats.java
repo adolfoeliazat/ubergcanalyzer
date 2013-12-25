8a41fa0aec7a798e95f39817bcccf952d737900d
Christian Bannes <cbannes@gmail.com>
2013-12-25 21:14:27 +0100

    provided class for math statistics (standard deviation, etc)

---
+++ src/main/java/de/morten/model/statistics/Stats.java
@@ -0,0 +1,109 @@
+package de.morten.model.statistics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * 
+ * statistics we want to show:
+ *  - total number of gc
+ *  - number percentage of one type to all gc	
+ *  - total gc secs
+ *  - total gc secs %
+ *  - overhead = timegc/timeall * 100 
+ *  - throughput = Throughput is the percentage of time available to the application for execution ( 1-  overhead)
+ *  - avg
+ *  - standard deviation
+ *  - min
+ *  - max
+ *  - 
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * Provides static methods for statistics calculation
+ * 
+ * @author Christian Bannes
+ */
+public class Stats {
+	
+	
+	/**
+	 * Private default constructor
+	 */
+	private Stats() {
+		//prevent instantiation
+	}
+	
+	/**
+	 * Returns the median of the given collection
+	 * 
+	 * @param values the values 
+	 * @return the median
+	 */
+	public static double median(final Collection<Integer> values)
+	{
+		if(values.isEmpty()) return 0;
+		
+		final List<Integer> list = new ArrayList<Integer>(values);
+		Collections.sort(list);
+		
+		final int mid = values.size() / 2;
+		return list.get(mid);
+	}
+	
+	/**
+	 * Calculates the variance over the given values
+	 * 
+	 * @param values the values
+	 * @return the variance
+	 */
+	public static double variance(final Collection<Integer> values) {
+		final double avg = Stats.average(values);
+		
+		if(values.isEmpty()) return 0;
+		
+		double sum = 0;
+		for(final int val : values)
+		{
+			sum += Math.pow((val - avg), 2);
+			
+		}
+		return sum / values.size();
+	}
+	
+	/**
+	 * Calculates the standard deviation over the given values
+	 * 
+	 * @param values thee values
+	 * @return the standard deviation
+	 */
+	public static double standardDeviation(final Collection<Integer> values) {
+		return Math.sqrt(Stats.variance(values));
+	}
+	
+	/**
+	 * Calculates the average value over the given values
+	 * 
+	 * @param values the values
+	 * @return the average
+	 */
+	public static double average(final Collection<Integer> values)
+	{
+		if(values.isEmpty()) return 0;
+		
+		int sum = 0;
+		for(final int value : values)
+		{
+			sum += value;
+		}
+		
+		return sum / values.size();
+	}
+}
