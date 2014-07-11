a6057af06fb9eca1eaeadbef858b5fb707a20ba6
Christian Bannes <cbannes@gmail.com>
2014-07-11 23:27:19 +0200

    add documentation

--- src/main/java/de/morten/model/parser/AbstractParser.java
+++ src/main/java/de/morten/model/parser/AbstractParser.java
@@ -10,6 +10,12 @@ import de.java.regexdsl.model.Regex;
 import de.morten.model.task.Message;
 import de.morten.model.task.MessageConsumer;
 
+/**
+ * Convenient implementation of a parser. A gc parser should always
+ * extend this class.
+ * 
+ * @author Christian Bannes
+ */
 public abstract class AbstractParser implements MessageConsumer {
 
 	private String buffer = new String();
@@ -21,6 +27,54 @@ public abstract class AbstractParser implements MessageConsumer {
 		
 		return isMultiLine()? consumeMultiLine(message) : consumeSingleLine(message);
 	}
+	
+	@Override
+	public void reset() {
+		this.buffer = new String();
+	}
+	
+	/**
+	 * Publish an event the given match
+	 * 
+	 * @param match the match
+	 * @param message the message that matches on some parser
+	 */
+	protected abstract void publishEventFor(Match match, Message message);
+	
+	/**
+	 * Determines if an gc event that is supposed to be detected can 
+	 * consist of multiple log lines
+	 * 
+	 * @return true, if the gc event can consist of multiple log lines, else false
+	 */
+	protected abstract boolean isMultiLine();
+	
+	/**
+	 * A parser begins consuming messages when it detects a start marker.
+	 * 
+	 * @param message the message containing one log line
+	 * @return true if the start marker is detected, else false.
+	 */
+	protected abstract boolean startParsing(Message message);
+	
+	/**
+	 * The regex pattern that describes the match of an event the parser is responsible
+	 * for, e.g. the regex to detect a FullGC.
+	 * 
+	 * @return the regex
+	 */
+	protected abstract Regex pattern();
+	
+	/**
+	 * Returns true if the parser is sure that this message cannot be 
+	 * the last line that makes up one event 
+	 * 
+	 * @param message the message
+	 * @return true, if the message is definitely not the last line of 
+	 * 			a gc event
+	 */
+	protected abstract boolean inlineDetected(Message message);
+	
 	
 	private boolean consumeSingleLine(final Message message)
 	{
@@ -64,21 +118,10 @@ public abstract class AbstractParser implements MessageConsumer {
 		return true;
 	}
 	
-	
-	protected abstract void publishEventFor(Match match, Message message);
-	protected abstract boolean isMultiLine();
-	protected abstract boolean startParsing(Message message);
-	protected abstract Regex pattern();
-	protected abstract boolean inlineDetected(Message message);
-	
-	
 
 	private boolean alreadyStarted() {
 		return this.buffer.length() > 0;
 	}
 
-	public void reset() {
-		this.buffer = new String();
-	}
 
 }
