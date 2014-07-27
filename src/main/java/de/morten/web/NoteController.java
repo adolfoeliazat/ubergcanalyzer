f8d4da2eed0e7945d0a92c533b41926e420fbd13
Christian Bannes <cbannes@gmail.com>
2014-07-27 18:22:06 +0200

    setup for container based security & include of persistence.xml

--- src/main/java/de/morten/web/NoteController.java
+++ src/main/java/de/morten/web/NoteController.java
@@ -15,15 +15,13 @@ public class NoteController implements Serializable {
 	private static final long serialVersionUID = 7199060576374141490L;
 	String text = "Enter your notes...";
 	private String fontFamily = "Satisfy";
-	private int fontSize = 16;
+	private int fontSize = 12;
 
 	public String getText() {
-		System.out.println("get text");
 		return this.text;
 	}
 	
 	public void setText(String text) {
-		System.out.println("setting text");
 		this.text = text;
 	}
 	
@@ -35,22 +33,16 @@ public class NoteController implements Serializable {
 
 	public void setFontFamily(final String fontFamily)
 	{
-		System.out.println("setting fontfamily:" + fontFamily);
 		this.fontFamily = fontFamily;
 	}
 	
 	public int getFontSize()
 	{
-		System.out.println("get fontsize");
 		return this.fontSize;
 	}
 
 	public void setFontSize(final int fontSize)
 	{
-	//	UIComponent component = UIComponent.getCurrentComponent(FacesContext.getCurrentInstance());
-	//	org.primefaces.component.selectonelistbox.SelectOneListbox box = (org.primefaces.component.selectonelistbox.SelectOneListbox) component;
-	//	box.getSubmittedValue();
-		System.out.println("setting fontSize:" + fontSize);
 		this.fontSize = fontSize;
 	}
 }
