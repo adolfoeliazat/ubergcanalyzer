f3ce75a8a2ed905500c40229b9688914ea898dcf
Christian Bannes <cbannes@gmail.com>
2014-07-28 22:46:45 +0200

    remove LogoutServlet, implement Users.logout instead

--- src/main/java/de/morten/identityaccess/Users.java
+++ src/main/java/de/morten/identityaccess/Users.java
@@ -1,17 +1,23 @@
 package de.morten.identityaccess;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Produces;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
 @ApplicationScoped
 @Named
 public class Users {
 	
-	@Produces
 	public User getCurrentUser() {
 		final String name = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName();
 		return name != null? new User(name) : new User("anonymous");
+	}
+	
+
+	public String logout() {
+		final HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		if(session != null)session.invalidate();
+		return "dashboard.xhtml?faces-redirect=true";
 	}
 }
